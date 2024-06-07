401300534536001192e6725e47abc2b9f1c9238e
Adrian Booth <adrian.booth17@gmail.com>
2024-06-07 15:03:31 -0500

    render bar chart of expenses

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,12 +1,37 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend);
 
 const HelloWorld = (props) => {
-  return (
-    <React.Fragment>
-      <h1>Greeting: {props.greeting}</h1>
-    </React.Fragment>
-  )
+  const labels = Object.keys(props.expenseData);
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: 'Expenses',
+        data: labels.map(date => props.expenseData[date].reduce((total, expense) => total + expense.amount, 0)),
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      x: {
+        type: 'category',
+      },
+      y: {
+        type: 'linear',
+        beginAtZero: true,
+      },
+    },
+  };
+  return <Bar data={data} options={options} />;
+  
 }
 
 HelloWorld.propTypes = {
