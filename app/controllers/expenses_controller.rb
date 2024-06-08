70ed2f2e37377bc045b824c6ebeea92b740ade87
Adrian Booth <adrian.booth17@gmail.com>
2024-06-07 19:48:52 -0500

    get bar, pie and table charts up

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,6 +7,23 @@ class ExpensesController < ApplicationController
     .where("DATE_PART('month', expense_date) = ?", Date.current.month)
     .where('expense_date <= ?', Date.today)
     .map {|e| e.attributes}.group_by {|expense| expense['expense_date'].strftime("%d/%m/%Y")}
+    monthly_expenses = @grouped_expenses.values.flatten.sum {|e| e['amount']}
+    grouped_by_category = Expense
+    .select("amount, expense_date, one_off, categories.title AS category_title")
+    .joins(:category)
+    .where("DATE_PART('month', expense_date) = ?", Date.current.month)
+    .where('expense_date <= ?', Date.today)
+    .map {|e| e.attributes}.group_by {|expense| expense['category_title']}
+
+    @grouped_by_category_percentages = grouped_by_category.transform_values do |expenses|
+
+      sum = expenses.sum { |expense| ((expense["amount"].to_f / monthly_expenses) * 100).to_f.round(1) }
+    end.sort_by  {|k,v| v}.reverse.to_h
+    @grouped_by_category_values = grouped_by_category.transform_values do |expenses|
+
+      sum = expenses.sum { |expense| expense['amount'] }
+    end.sort_by  {|k,v| v}.reverse.to_h
+    @expenses_today = @grouped_expenses[Date.today.strftime("%d/%m/%Y")].sum {|e| e['amount']}
   end
 
 end
--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -1,13 +1,43 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { Bar } from 'react-chartjs-2';
+import { Bar, Pie } from 'react-chartjs-2';
 import annotationPlugin from 'chartjs-plugin-annotation';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
-
-ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip, Legend, annotationPlugin);
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Chart as ChartJS, ArcElement, CategoryScale, LinearScale, BarElement, Tooltip, Legend } from 'chart.js';
+ChartJS.register(ArcElement, CategoryScale, LinearScale, BarElement, Tooltip, Legend, annotationPlugin, ChartDataLabels);
 
 const HelloWorld = (props) => {
+  ChartJS.overrides.pie.plugins.legend.display = false
+  ChartJS.defaults.set('plugins.datalabels', {
+    display: false
+  });
   const labels = Object.keys(props.expenseData);
+  console.log(Object.values(props.categoryData))
+  const pieData = {
+    labels: Object.keys(props.categoryData),
+    datasets: [
+      {
+        data: Object.values(props.categoryData),
+        backgroundColor: [
+          '#FF6384', // Red
+          '#36A2EB', // Blue
+          '#FFCE56', // Yellow
+          '#8BC34A', // Green
+          '#9370DB', // Purple
+          '#FF9800', // Orange
+          '#4FC3F7', // Light Blue
+          '#E57373', // Red Shade
+          '#81C784', // Green Shade
+          '#BA68C8', // Purple Shade
+          '#FFB74D', // Orange Shade
+          '#4DD0E1', // Cyan
+          '#E57373', // Red Shade
+          '#81C784', // Green Shade
+          '#BA68C8'  // Purple Shade
+        ]
+      },
+    ],
+  };
 
   const data = {
     labels,
@@ -18,6 +48,23 @@ const HelloWorld = (props) => {
         backgroundColor: 'rgba(75, 192, 192, 0.6)',
       },
     ],
+  };
+
+  const pieOptions = {
+    plugins: {
+      datalabels: {
+        formatter: function(value, context) {
+          return value.toFixed(2).toString() + "%";
+        },
+        backgroundColor: '#ffffff',
+        display: true,
+        align: 'bottom',
+        borderRadius: 3,
+        font: {
+          size: 10,
+        },
+      },
+    },
   };
 
   const options = {
@@ -49,7 +96,28 @@ const HelloWorld = (props) => {
     }
   }
   };
-  return <Bar data={data} options={options} />;
+  return(
+    <div>
+      <Bar data={data} options={options} />;
+      <Pie data={pieData} options={pieOptions} />;
+      <table className="expenses-table">
+        <thead>
+          <tr>
+            <th>Category</th>
+            <th>Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(props.categoryValues).map(([category, amount]) => (
+            <tr key={category}>
+              <td>{category}</td>
+              <td>${amount.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
 
 }
 
